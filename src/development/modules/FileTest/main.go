938fbd75f2089bab3806c617436b1a471f6ef9ff
Timothy Laskoski <timothy.laskoski@gmail.com>
2015-12-05 15:41:44 -0500

    save

---
+++ src/development/modules/FileTest/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	u "github.com/whitman-colm/go-1/utils/other"
+	io "io/ioutil"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	test, err := strconv.Atoi(LastLine("test.txt"))
+	u.QuitAtError(err)
+	fmt.Println(test)
+}
+
+func LastLine(fname string) string {
+	data, err := io.ReadFile(fname)
+	if err != nil {
+		panic(err)
+	}
+
+	sdata := string(data)
+	spdata := strings.Split(sdata, "\n")
+	lline := spdata[len(spdata)-2]
+	return lline
+}
