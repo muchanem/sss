17940f206ed6463a22806ff976c213e480619354
Timothy Laskoski <timothy.laskoski@gmail.com>
2015-12-05 16:11:23 -0500

    save

--- src/development/modules/UUID/main.go
+++ src/development/modules/UUID/main.go
@@ -7,18 +7,6 @@ import (
 	"strings"
 )
 
-<<<<<<< HEAD
-=======
-func main() {
-	x, err := strconv.Atoi(LastLine("test.txt"))
-
-
-func WriteUUID(fname string) {
-	err := io.WriteFile(fname)
-
-}
-
->>>>>>> bdd4fdbf9e3bcc1c8ed06ad2f7c510e23fcc6551
 func LastLine(fname string) int {
 	data, err := io.ReadFile(fname)
 	u.QuitAtError(err)
