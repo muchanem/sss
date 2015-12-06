0ecc239d331eb680627e9bccc68590a72c9c47d3
Timothy Laskoski <timothy.laskoski@gmail.com>
2015-12-06 13:36:11 -0500

    save

--- src/development/modules/UITest/main.go
+++ src/development/modules/UITest/main.go
@@ -1,6 +1,8 @@
-package nil
+package main
 
 import (
+	id "development/modules/UUID"
+	e "development/modules/email"
 	"fmt"
 	c "github.com/skilstak/go/colors"
 	i "github.com/whitman-colm/go-1/utils/input"
@@ -11,9 +13,9 @@ func Output() {
 	fmt.Println(c.B3 + "║ " + c.Y + "SkilStak Support System, By: " + c.G + "@whitman-colm          " + c.B3 + "║")
 	fmt.Println(c.B3 + "║ " + c.O + "This is run by the following modules:               " + c.B3 + "║")
 	fmt.Println(c.B3 + "║                                                     ║")
-	fmt.Println(c.B3 + "║ " + c.V + "SSS Main  |  " + c.X + "Version β-S-3.1.0                      " + c.B3 + "║")
-	fmt.Println(c.B3 + "║ " + c.V + "SSS Email |  " + c.X + "Version β-S-4.2.0                      " + c.B3 + "║")
-	fmt.Println(c.B3 + "║ " + c.V + "SSS UUID  |  " + c.X + "Version ß-S-1.0.0                      " + c.B3 + "║")
+	fmt.Println(c.B3 + "║ " + c.V + "SSS Main  |  " + c.X + "V β-S-3.1.0                      " + c.B3 + "║")
+	fmt.Println(c.B3 + "║ " + c.V + "SSS Email |  " + c.X + e.Version + "                      " + c.B3 + "║")
+	fmt.Println(c.B3 + "║ " + c.V + "SSS UUID  |  " + c.X + id.Version + "                      " + c.B3 + "║")
 	fmt.Println(c.B3 + "╚═════════════════════════════════════════════════════╝")
 	fmt.Println()
 	fmt.Println(c.M + "How can I help you today?")
