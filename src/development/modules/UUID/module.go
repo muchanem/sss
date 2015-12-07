d873d76f4cb6e9bb7b2efdba89c88623aa9d0975
Whitman Huntley <huntleywhitman@gmail.com>
2015-12-07 13:06:18 -0500

    save

--- src/development/modules/UUID/module.go
+++ src/development/modules/UUID/module.go
@@ -7,28 +7,30 @@ import (
 	"strings"
 )
 
-const Version string = "\033[1;35mV ß-S-1.0.4"
+const Version string = "\033[1;35mV ß-S-1.0.5"
 
 func WriteUUID(fname string, data int) {
-	bconv := strings.Split(stringconv.Itoa(data), "")
-	bl := []byte{}
-	for _, asc := range bconv {
-		blr := append(byte(bconv), bl)
-	}
+	bconv := /*strings.Split(*/ strconv.Itoa(data) /*, "")*/
+	//Defines bconv as an array of strings from the int data, so 1020 becomes "1","0","2","0"
+	//bl := []byte{}
+	//makes bl an empty byte slice. A slice is golang's array with no limit on how many number
+	blr := []byte(bconv)
+	//define blr outside of the loop. the layer in which a constant is defined is the lowest layer it can be
+	//used.
+	/*for _, asc := range bconv {
+		blr = append([]byte(bconv), bl)
+		//appends
+	}*/
 	io.WriteFile(fname, blr, 0664)
 }
 
 func LastLine(fname string) int {
 	data, err := io.ReadFile(fname)
-	if err != nil {
-		u.QuitAtError(err)
-	}
+	u.QuitAtError(err)
 	sdata := string(data)
 	spdata := strings.Split(sdata, "\n")
 	lline := spdata[len(spdata)-2]
 	uuid, err := strconv.Atoi(lline)
-	if err != nil {
-		u.QuitAtError(err)
-	}
+	u.QuitAtError(err)
 	return uuid
 }
--- src/development/modules/email/module.go
+++ src/development/modules/email/module.go
@@ -100,7 +100,7 @@ func nonUrgent(user string, priority string) {
 			fmt.Println(c.B2, "Sorry M9, thats not a valid statment...")
 		}
 	}
-	uuid := id.LastLine("module/UUID/listOfUUID.txt")
+	uuid := id.LastLine("listOfUUID.txt")
 	to := []string{"skilstakta@gmail.com"}
 	msg := []byte("To: skilstakta@gmail.com\r\n" +
 		"Subject: TICKET #" + string(uuid) + ".  Hey, I'm " + user + ". I need some help with " + lang + ". For context, it's " + priority + " priority.\r\n" +
@@ -112,8 +112,7 @@ func nonUrgent(user string, priority string) {
 	s.QuitAtError(err)
 	s.Go(0)
 	fmt.Println(c.B3 + "Sent! Help is on its way!")
-	newUuid := []byte{byte(uuid + 1)}
-	id.WriteUUID("listOfUUID.txt", newUuid)
+	id.WriteUUID("listOfUUID.txt", uuid)
 }
 
 func devurgent(user string) {
